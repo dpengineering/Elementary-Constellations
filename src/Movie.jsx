a5472b7bf65983dccb88f726a39b985eb301e2b8
Phillip <phillipghodgson@gmail.com>
2025-03-20 09:19:13 -0700

    forcing rear camera to see if that works

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -22,7 +22,7 @@ export function Movie({ threshold, ref }) {
     navigator.mediaDevices
       .getUserMedia({
         video: "true",
-        facingMode: "environment",
+        facingMode: { exact: "environment" },
         // width: { min: 640, exact: 640, max: 640 },
         // height: { exact: 480 },
         aspectRatio: 1.33333333333,
